e7bd7e99cdd8be40dc0d9e08cfed8b40f7f28719
xiaobaowan1988 <baoliangwan1988@gmail.com>
2020-08-19 21:48:04 +0800

    pb

--- server.go
+++ server.go
@@ -3,15 +3,23 @@ package main
 import (
   "log"
   "net"
+  "github.com/xiaobaowan1988/gopractice2/chat"
   "google.golang.org/grpc"
+  "fmt"
 )
 
 func main() {
-  lis, err := net.Listen("tcp",":9000")
+  fmt.Println("GoGO")
+
+  lis, err := net.Listen("tcp",fmt.Sprintf(":%d",9000))
   if err != nil {
     log.Fatalf("failed to listen: %v", err)
   }
+
+  s := chat.Server{}
+
   grpcServer := grpc.NewServer()
+  chat.RegisterChatServiceServer(grpcServer, &s)
 
   if err := grpcServer.Serve(lis); err != nil {
     log.Fatalf("failed to server: %s", err)
