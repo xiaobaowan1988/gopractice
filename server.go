ea1dee0d8b11b24e94325e8eb2559a3e14eb3f49
Ubuntu <azureuser@golearningvm.a1mq3ipn3q3e1jrjzakjnkna1f.bx.internal.cloudapp.net>
2020-08-20 12:47:55 +0000

    refactor

--- server.go
+++ server.go
@@ -6,7 +6,18 @@ import (
   "github.com/xiaobaowan1988/gopractice2/chat"
   "google.golang.org/grpc"
   "fmt"
+  "context"
 )
+
+
+type Server struct {
+}
+
+func (s *Server) SayHello(ctx context.Context, in *chat.Message) (*chat.Message, error) {
+  log.Printf("Receive message body from client: %s", in.Body)
+  return    &chat.Message{Body: "Hello From the Server!"}, nil
+}
+
 
 func main() {
   fmt.Println("GoGO")
